09a012346e9ac238ec4a63f8da41bde196d6c120
taeyanglim <taeyang7704@naver.com>
2021-11-14 00:06:28 +0900

    init project

---
+++ src/test/java/com/giraffelim/SpringbootShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giraffelim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShoppingMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
