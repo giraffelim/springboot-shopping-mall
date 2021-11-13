b5595d0d6744fd071c290cc20c085b79d54aae87
taeyanglim <taeyang7704@naver.com>
2021-11-14 00:40:08 +0900

    feat: add BaseEntity for use JPA Auditing

---
+++ src/main/java/com/giraffelim/entity/audit/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.giraffelim.entity.audit;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime regTime;  // 등록시간
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;   // 수정시간
+}
