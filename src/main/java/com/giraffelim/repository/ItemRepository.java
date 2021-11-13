1269aaf27608efde05c965c6616b294fd4071850
taeyanglim <taeyang7704@naver.com>
2021-11-14 01:22:38 +0900

    test: add queryMethod(by, or, lessThan, orderBy) test in ItemRepositoryTest

--- src/main/java/com/giraffelim/repository/ItemRepository.java
+++ src/main/java/com/giraffelim/repository/ItemRepository.java
@@ -3,5 +3,13 @@ package com.giraffelim.repository;
 import com.giraffelim.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+    List<Item> findByPriceLessThan(Integer price);
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
 }
--- src/test/java/com/giraffelim/repository/ItemRepositoryTest.java
+++ src/test/java/com/giraffelim/repository/ItemRepositoryTest.java
@@ -2,12 +2,16 @@ package com.giraffelim.repository;
 
 import com.giraffelim.constant.ItemSellStatus;
 import com.giraffelim.entity.Item;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @TestPropertySource(locations = "classpath:application-test.yml")
@@ -27,7 +31,68 @@ class ItemRepositoryTest {
         item.setStockNumber(100);
 
         Item savedItem = itemRepository.save(item);
-        Assertions.assertThat(savedItem).isEqualTo(item);
+        assertThat(savedItem).isEqualTo(item);
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    void find_by_item_nm_test() {
+        List<Item> findItemList = itemRepository.findByItemNm("테스트 상품1");
+        for (Item item : findItemList) {
+            System.out.println(item.toString());
+            assertThat(item.getItemNm()).isEqualTo("테스트 상품1");
+        }
+    }
+
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    void find_by_item_nm_or_item_detail_test() {
+        List<Item> findItemList = itemRepository
+                .findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+
+        for (Item item : findItemList) {
+            System.out.println(item.toString());
+        }
+
+        assertThat(findItemList).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    void find_by_price_less_than_test() {
+        List<Item> findItemList = itemRepository.findByPriceLessThan(10005);
+
+        for (Item item : findItemList) {
+            System.out.println(item.toString());
+        }
+
+        assertThat(findItemList).hasSize(4);
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    void find_by_price_less_than_order_by_price_desc_test() {
+        List<Item> findItemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+
+        for (Item item : findItemList) {
+            System.out.println(item);
+        }
+
+        assertThat(findItemList).hasSize(4);
+    }
+
+    @BeforeEach
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+
+            itemRepository.save(item);
+        }
     }
 
 }
