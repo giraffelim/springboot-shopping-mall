9fb8f71631687480e76ce337ba39230e6745b79b
taeyanglim <taeyang7704@naver.com>
2021-11-14 23:04:54 +0900

    refactor: 기존 BaseEntity를 BaseTimeEntity로 변경

--- src/main/java/com/giraffelim/entity/audit/BaseTimeEntity.java
+++ src/main/java/com/giraffelim/entity/audit/BaseTimeEntity.java
@@ -1,6 +1,7 @@
 package com.giraffelim.entity.audit;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -10,9 +11,10 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
+public abstract class BaseTimeEntity {
 
     @CreatedDate
     private LocalDateTime regTime;  // 등록시간
