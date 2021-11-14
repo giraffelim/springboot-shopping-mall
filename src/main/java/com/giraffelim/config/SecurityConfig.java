66bd9f671fb3ada45f6f9c64a663611633836d60
taeyanglim <taeyang7704@naver.com>
2021-11-14 21:37:50 +0900

    feat: CustomAuthenticationEntryPoint 및 static 하위 디렉터리 시큐리티 무시 설정, URI별 ROLE 설정 추가

--- src/main/java/com/giraffelim/config/SecurityConfig.java
+++ src/main/java/com/giraffelim/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,10 +30,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .logout()
                     .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
-                    .logoutSuccessUrl("/")
-                .and()
-                    .authorizeRequests()
-                    .mvcMatchers("/", "/members/**").permitAll();
+                    .logoutSuccessUrl("/");
+
+        http
+                .authorizeRequests()
+                    .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
+                    .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+        http
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**");
     }
 
     @Override
