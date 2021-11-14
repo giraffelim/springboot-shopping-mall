d83d70cd3a2d4c9245883934db010ea78240bb97
taeyanglim <taeyang7704@naver.com>
2021-11-14 23:10:19 +0900

    test: 영속성 전이, 고아객체 제거, 지연로딩 테스트를 포함한 OrderTest 추가

---
+++ src/test/java/com/giraffelim/entity/OrderTest.java
@@ -0,0 +1,118 @@
+package com.giraffelim.entity;
+
+import com.giraffelim.constant.ItemSellStatus;
+import com.giraffelim.repository.ItemRepository;
+import com.giraffelim.repository.MemberRepository;
+import com.giraffelim.repository.OrderItemRepository;
+import com.giraffelim.repository.OrderRepository;
+import org.hibernate.proxy.HibernateProxy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@TestPropertySource(locations = "classpath:application-test.yml")
+public class OrderTest {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    OrderItemRepository orderItemRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    private Item createItem() {
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("상세설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        return item;
+    }
+
+    private Order createOrder() {
+        Order order = new Order();
+
+        for (int i = 0; i < 3; i++) {
+            Item item = createItem();
+            itemRepository.save(item);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(1000);
+
+            order.addOrderItem(orderItem);
+        }
+
+        Member member = new Member();
+        memberRepository.save(member);
+
+        order.setMember(member);
+        orderRepository.save(order);
+
+        return order;
+    }
+
+    @Test
+    @DisplayName("영속성 전이 테스트")
+    void cascade_test() {
+        Order order = new Order();
+
+        for (int i = 0; i < 3; i++) {
+            Item item = this.createItem();
+            itemRepository.save(item);
+
+            OrderItem orderItem = new OrderItem();
+            orderItem.setItem(item);
+            orderItem.setCount(10);
+            orderItem.setOrderPrice(1000);
+
+            order.addOrderItem(orderItem);
+        }
+
+        orderRepository.saveAndFlush(order);
+        em.clear();
+
+        Order savedOrder = orderRepository.findById(order.getId()).orElseThrow(EntityNotFoundException::new);
+        assertThat(savedOrder.getOrderItems()).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("고아객체 제거 테스트")
+    void orphan_removal_test() {
+        Order order = this.createOrder();
+        order.getOrderItems().remove(0);
+        em.flush();
+    }
+
+    @Test
+    @DisplayName("지연 로딩 테스트")
+    void lazy_loading_test() {
+        Order order = this.createOrder();
+        Long orderItemId = order.getOrderItems().get(0).getId();
+        em.flush();
+        em.clear();
+
+        OrderItem orderItem = orderItemRepository.findById(orderItemId).orElseThrow(EntityNotFoundException::new);
+        assertThat(orderItem.getOrder()).isInstanceOf(HibernateProxy.class);
+    }
+
+}
