7f77e4a03001c3ad67d98d98a8975365f94b6487
taeyanglim <taeyang7704@naver.com>
2021-11-14 21:44:04 +0900

    test: 상품 등록 페이지 권한별 접근 테스트를 포함한 ItemControllerTest 추가

---
+++ src/test/java/com/giraffelim/controller/ItemControllerTest.java
@@ -0,0 +1,42 @@
+package com.giraffelim.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.yml")
+class ItemControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("상품 등록 페이지 권한 테스트")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    void item_form_test() throws Exception {
+        mockMvc.perform(get("/admin/item/new"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("상품 등록 페이지 일반 회원 접근 테스트")
+    @WithMockUser(username = "user", roles = "USER")
+    void item_form_not_admin_test() throws Exception {
+        mockMvc.perform(get("/admin/item/new"))
+                .andDo(print())
+                .andExpect(status().isForbidden());
+    }
+
+}
