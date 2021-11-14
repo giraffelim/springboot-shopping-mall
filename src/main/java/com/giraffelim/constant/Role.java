ef6805423584f95dcea3b8b640b61e867ae28f17
taeyanglim <taeyang7704@naver.com>
2021-11-14 19:08:30 +0900

    feat: Member Entity와 Reposiotry, Service 추가

---
+++ src/main/java/com/giraffelim/constant/Role.java
@@ -0,0 +1,5 @@
+package com.giraffelim.constant;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
---
+++ src/main/java/com/giraffelim/entity/Member.java
@@ -0,0 +1,45 @@
+package com.giraffelim.entity;
+
+import com.giraffelim.constant.Role;
+import com.giraffelim.dto.MemberFormDto;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "MEMBER")
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    @Id
+    @Column(name = "MEMBER_ID")
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder) {
+        Member member = new Member();
+        member.setName(memberFormDto.getName());
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+        member.setPassword(passwordEncoder.encode(memberFormDto.getPassword()));
+        member.setRole(Role.ROLE_USER);
+        return member;
+    }
+}
