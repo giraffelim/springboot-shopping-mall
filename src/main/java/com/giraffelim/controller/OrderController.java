50a6a52a3934aaf2be0396c00d47e762e871ae3d
giraffelim <taeyang7704@naver.com>
2021-11-19 13:25:25 +0900

    feat: 주문 목록 조회 기능 추가

--- src/main/java/com/giraffelim/controller/OrderController.java
+++ src/main/java/com/giraffelim/controller/OrderController.java
@@ -1,20 +1,23 @@
 package com.giraffelim.controller;
 
 import com.giraffelim.dto.OrderDto;
+import com.giraffelim.dto.OrderHistDto;
 import com.giraffelim.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,8 +26,9 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/order")
-    public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto orderDto,
-                                              BindingResult bindingResult, Principal principal) {
+    public @ResponseBody
+    ResponseEntity order(@RequestBody @Valid OrderDto orderDto,
+                         BindingResult bindingResult, Principal principal) {
 
         if (bindingResult.hasErrors()) {
             StringBuilder sb = new StringBuilder();
@@ -45,6 +49,19 @@ public class OrderController {
         }
 
         return new ResponseEntity(orderId, HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"/orders", "/orders/{page}"})
+    public String orderHist(@PathVariable("page") Optional<Integer> page, Principal principal, Model model) {
+        PageRequest pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
+
+        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        model.addAttribute("orders", orderHistDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+
+        return "order/orderHist";
     }
 
 }
