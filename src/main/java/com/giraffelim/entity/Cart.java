dffa4eb077c55dac893d3db40ccd5e7eae52fc69
giraffelim <taeyang7704@naver.com>
2021-11-26 18:05:05 +0900

    feat: 장바구니 기능 추가

--- src/main/java/com/giraffelim/entity/Cart.java
+++ src/main/java/com/giraffelim/entity/Cart.java
@@ -23,4 +23,10 @@ public class Cart extends BaseTimeEntity {
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
+    public static Cart createCart(Member member) {
+        Cart cart = new Cart();
+        cart.setMember(member);
+        return cart;
+    }
+
 }
